5eec4a07a253189483ed67b1aa1e3f8f5ab66825
marlon-azevedo <48135703+marlon-azevedo@users.noreply.github.com>
2019-04-02 10:40:10 -0300

    Update exercicio_11.php

--- exercicio_11.php
+++ exercicio_11.php
@@ -1,6 +1,12 @@
 <?php
 
-print "digite um número entre 0-1000: \n";
+/*11. Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas,
+dezenas e unidades do mesmo, observando os termos no plural a colocação do "e", da vírgula entre
+outros. Exemplo:
+326 = 3 centenas, 2 dezenas e 6 unidades
+12 = 1 dezena e 2 unidades*/
+
+print "Digite um número entre 0-1000: ";
 $Numero= fgets(STDIN);
 
 $qtnumero= $Numero;
