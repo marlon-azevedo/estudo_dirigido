9221cec97adbf8aa0df83eae9225ef15aa6643fc
marlon-azevedo <48135703+marlon-azevedo@users.noreply.github.com>
2019-04-02 10:49:12 -0300

    Update exercicio_17.php

--- exercicio_17.php
+++ exercicio_17.php
@@ -1,5 +1,8 @@
 <?php
 
+/*17. Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado
+ao segundo número. Não utilize a função de potência da linguagem.*/
+
 print "Digite o 1°Número: ";
 $Base=fgets(STDIN);
 
