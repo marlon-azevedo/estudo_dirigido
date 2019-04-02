7d4885e394f031dc52baa98508da19b05ea89764
marlon-azevedo <48135703+marlon-azevedo@users.noreply.github.com>
2019-04-02 10:43:23 -0300

    Update exercicio_12.php

--- exercicio_12.php
+++ exercicio_12.php
@@ -1,10 +1,11 @@
 <?php
+//12. Faça um algoritmo que mostre os números de 1 a 100;
 
     $contador=0;
 
     while ($contador<100) {
 
         $contador++;
-        print "$contador ";
+        print "$contador \n";
 
     }
