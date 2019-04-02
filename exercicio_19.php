09d1d0741eb3e6f2d9b0d23fbbee392af3639ad4
marlon-azevedo <48135703+marlon-azevedo@users.noreply.github.com>
2019-04-02 10:50:13 -0300

    Update exercicio_19.php

--- exercicio_19.php
+++ exercicio_19.php
@@ -1,5 +1,7 @@
 <?php
 
+/*19. A série de Fibonacci é formada pela sequência 0,1,1,2,3,5,8,13,21,34,55, [...] Faça um programa que
+gere a série até que o valor seja maior que 500.*/
 
 print "Deseja Fibonacciar até quanto? ";
 $fibonacciar= (float) fgets(STDIN);
